f99e1b3bdb31d1ac7e57297e1f95a7d2dfd584e7
Müşerref Selçuk Özdemir <s_05_60@hotmail.com>
2022-12-02 22:11:25 +0300

    nabeeer

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
 });
 
 const signup = async (req, res, next) => {
-  let invalidPassMessage = "Password must have: \n";
+  // let invalidPassMessage = "Password must have: \n";
   const { name, email, password } = req.body;
 
   let existingUser = await User.findOne({ email: req.body.email }); // Find user by email
@@ -26,36 +26,36 @@ const signup = async (req, res, next) => {
     return res.status(400).json({ message: "User already exists!" }); // Return error if user already exists
   }
 
-  var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  // var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
-  if (
-    !password.match(/^(?=.*\d)/gm) ||
-    !password.match(/^(?=.*[a-z])/gm) ||
-    !password.match(/^(?=.*[A-Z])/gm) ||
-    !password.match(format) ||
-    password.length < 6
-  ) {
-    if (!password.match(/^(?=.*\d)/gm)) {
-      invalidPassMessage = invalidPassMessage + "1 number!\n";
-    }
-    // Check if the password have lowercase letter
-    if (!password.match(/^(?=.*[a-z])/gm)) {
-      invalidPassMessage = invalidPassMessage + "1 lowercase letter!\n";
-    }
-    // Check if the password have uppercase letter
-    if (!password.match(/^(?=.*[A-Z])/gm)) {
-      invalidPassMessage = invalidPassMessage + "1 uppercase letter!\n";
-    }
-    // Check if the password have 8 characters
-    if (password.length < 8) {
-      invalidPassMessage = invalidPassMessage + "atleast 8 characters!\n";
-    }
+  // if (
+  //   !password.match(/^(?=.*\d)/gm) ||
+  //   !password.match(/^(?=.*[a-z])/gm) ||
+  //   !password.match(/^(?=.*[A-Z])/gm) ||
+  //   !password.match(format) ||
+  //   password.length < 6
+  // ) {
+  //   if (!password.match(/^(?=.*\d)/gm)) {
+  //     invalidPassMessage = invalidPassMessage + "1 number!\n";
+  //   }
+  //   // Check if the password have lowercase letter
+  //   if (!password.match(/^(?=.*[a-z])/gm)) {
+  //     invalidPassMessage = invalidPassMessage + "1 lowercase letter!\n";
+  //   }
+  //   // Check if the password have uppercase letter
+  //   if (!password.match(/^(?=.*[A-Z])/gm)) {
+  //     invalidPassMessage = invalidPassMessage + "1 uppercase letter!\n";
+  //   }
+  //   // Check if the password have 8 characters
+  //   if (password.length < 8) {
+  //     invalidPassMessage = invalidPassMessage + "atleast 8 characters!\n";
+  //   }
 
-    if (!format.test(password)) {
-      invalidPassMessage = invalidPassMessage + "1 special character!\n";
-    }
-    return res.status(400).json({ message: invalidPassMessage });
-  }
+  //   if (!format.test(password)) {
+  //     invalidPassMessage = invalidPassMessage + "1 special character!\n";
+  //   }
+  //   return res.status(400).json({ message: invalidPassMessage });
+  // }
 
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
